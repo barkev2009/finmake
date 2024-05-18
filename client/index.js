1d50bc2b9160678dadff19c4075b02db6353d7e2
Greg Kovshov <barkev2009@bk.ru>
2024-05-18 19:30:15 +0700

    new server

---
+++ client/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Wedding App Client started on port ${PORT}`));
