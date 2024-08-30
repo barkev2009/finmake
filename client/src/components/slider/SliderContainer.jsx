bcbeef42a59414015ddf202acf97851572574983
Greg Kovshov <barkev2009@bk.ru>
2024-08-31 01:59:03 +0700

    stylized sliders

--- client/src/components/slider/SliderContainer.jsx
+++ client/src/components/slider/SliderContainer.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useRef } from 'react'
+import { useState } from 'react';
 
 const SliderContainer = ({ length = 5, id, step = 1 }) => {
 
     const isClicked = useRef(false);
+    const [value, setValue] = useState(0);
+    const [selectorValue, setSelectorValue] = useState(0);
     const isVisible = useRef(document.querySelector(`#${id}.range-body`));
     useEffect(
         () => {
@@ -19,8 +22,33 @@ const SliderContainer = ({ length = 5, id, step = 1 }) => {
                     'touchend',
                     upHandler
                 );
+                document.querySelector(`#${id} .range-selector`).addEventListener(
+                    'mouseenter',
+                    () => {
+                        document.querySelector(`#${id} .range-selector .tooltip`).style.opacity = `100%`;
+                    }
+                );
+                document.querySelector(`#${id} .range-selector`).addEventListener(
+                    'mouseleave',
+                    () => {
+                        document.querySelector(`#${id} .range-selector .tooltip`).style.opacity = `0%`;
+                    }
+                );
             }
         }, isVisible.current
+    );
+    useEffect(
+        () => {
+            if (selectorValue) {
+                document.querySelector(`#${id} .range-selector`).style.left = `${selectorValue}%`;
+                document.querySelector(`#${id} .range-outer`).style.width = `${selectorValue}%`;
+                if (selectorValue * length / 100 - Math.floor(selectorValue * length / 100) > 0.95) {
+                    setValue(Math.floor(selectorValue * length / 100) + 1);
+                } else {
+                    setValue(Math.floor(selectorValue * length / 100));
+                }
+            }
+        }, [selectorValue]
     );
 
     const setPerc = (current) => {
@@ -34,8 +62,7 @@ const SliderContainer = ({ length = 5, id, step = 1 }) => {
             current = right;
             isClicked.current = false;
         }
-        const perc = (current - left) / (right - left) * 100;
-        document.querySelector(`#${id} .range-selector`).style.left = `${perc}%`;
+        setSelectorValue((current - left) / (right - left) * 100);
     }
 
     const sliderHandler = (e) => {
@@ -45,22 +72,26 @@ const SliderContainer = ({ length = 5, id, step = 1 }) => {
     }
     const downHandler = (e) => {
         isClicked.current = true;
+        document.querySelector(`#${id} .range-selector .tooltip`).style.opacity = `100%`;
         setPerc((e.touches !== undefined && e.touches.length > 0) ? e.touches[0].clientX : e.clientX);
     }
     const upHandler = (e) => {
         isClicked.current = false;
+        document.querySelector(`#${id} .range-selector .tooltip`).style.opacity = `0%`;
         setPerc((e.touches !== undefined && e.touches.length > 0) ? e.touches[0].clientX : e.clientX);
     }
 
     return (
         <div id={id} className='range-body' onMouseMove={sliderHandler} onMouseDown={downHandler} onMouseUp={upHandler}>
-        {/* <div id={id} className='range-body'> */}
+            {/* <div id={id} className='range-body'> */}
             <div className="range-inner"></div>
             <div className="range-outer"></div>
-            <div className="range-selector"></div>
+            <div className="range-selector">
+                <div className="tooltip">{value}</div>
+            </div>
             <div className="slider-line">
                 {
-                    [...Array(length / step + 1).keys()].map(i => <span key={i}>{i * step}</span>)
+                    [...Array(length / step + 1).keys()].map(i => <span style={{ left: `${i * step / length * 100}%`, right: `${i * step === length ? 0 : ''}%` }} key={i}>{i * step}</span>)
                 }
             </div>
         </div >
