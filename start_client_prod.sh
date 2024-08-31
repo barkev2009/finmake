46ad5d34379f2a3469bc94f01fca93c87d4051d6
Greg Kovshov <barkev2009@bk.ru>
2024-09-01 00:34:48 +0700

    nginx

--- start_client_prod.sh
+++ start_client_prod.sh
@@ -2,6 +2,6 @@ cd client
 echo "Installing client deps..."
 npm i
 npm run build
-# rm -r /var/www/build
-# mv build /var/www
-nohup node index.js &
+rm -r /var/www/build
+mv build /var/www
+# nohup node index.js &
