939b757a2e4cd77d7a0c8615b544b7f928d245a6
Greg Kovshov <barkev2009@bk.ru>
2024-11-17 17:16:21 +0700

    added ability to show commits in ADMIN

--- client/src/routes/Admin.jsx
+++ client/src/routes/Admin.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { $authHost } from '../api';
 import { exportHandler } from '../handlers';
+import { Octokit } from 'octokit';
 
 const Admin = () => {
+
+  const [commits, setCommits] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [total, setTotal] = useState(0);
 
   const downloadHandler = () => {
     $authHost.get('api/callRequest').then(
@@ -12,10 +17,66 @@ const Admin = () => {
     )
   }
 
+  const calcTime = (date1, date2) => {
+    const hours = (date1 - date2) / (1000 * 60 * 60);
+    if (hours < 24) {
+      return `${Math.round(hours)} hours ago`
+    } else {
+      return `${Math.round(hours / 24)} days ago`
+    }
+  }
+
+  useEffect(
+    () => {
+      let items = []
+      const octokit = new Octokit({ auth: process.env.REACT_APP_GITHUB_TOKEN });
+      octokit.request("GET /repos/{owner}/{repo}/commits", {
+        owner: process.env.REACT_APP_GITHUB_OWNER,
+        repo: process.env.REACT_APP_GITHUB_REPO,
+        per_page: 10
+      }).then(
+        async ({ data }) => {
+          setTotal(data.length);
+          for (let i = 0; i < data.length; i++) {
+            setCounter(i + 1);
+            const resp = await octokit.request('GET /repos/{owner}/{repo}/commits/{ref}',
+              {
+                owner: process.env.REACT_APP_GITHUB_OWNER,
+                repo: process.env.REACT_APP_GITHUB_REPO,
+                ref: data[i].sha
+              }
+            );
+            if (items.filter(j => j.sha === data[i].sha).length === 0) {
+              items.push(resp.data);
+            }
+          }
+          return items;
+        }
+      ).then(() => setCommits(items));
+    }, []
+  );
+
   return (
-    <div>
+    <div className='admin_container'>
       <h1>ADMIN</h1>
       <button onClick={downloadHandler}>Скачать отчет</button>
+      <h2>Last 10 commits</h2>
+      <div className="commits_container">
+        {commits.length === 0 ? <h3>{`Loading... (${counter} / ${total})`}</h3> :
+          commits.map((commit, idx) => <div key={idx} className='commit'>
+            <h3>{commit.commit.message}</h3>
+            <div>
+              {commit.files.map(file => <div key={file.sha}>
+                <span>{file.filename}</span>
+                <span className='additions'>{` +${file.additions} `}</span>
+                <span>/</span>
+                <span className='deletions'>{` -${file.deletions}`}</span>
+              </div>)}
+            </div>
+            <p>{calcTime(new Date(), new Date(commit.commit.author.date))}</p>
+          </div>)
+        }
+      </div>
     </div>
   )
 }
--- server/utils/generateJWT.js
+++ server/utils/generateJWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports =  function (login) {
-    let expiresIn = '5m';
+    let expiresIn = '7d';
     // if (role === 'ADMIN') {
     //     expiresIn = '1h';
     // }
