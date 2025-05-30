450c29419ec3baafa5ae134106ac5d432bb22535
Greg Kovshov <barkev2009@bk.ru>
2025-05-30 22:54:55 +0700

    stop timer

--- server/routers/index.js
+++ server/routers/index.js
@@ -3,10 +3,10 @@ const router = new Router();
 
 const callRequestRouter = require('./callRequestRouter');
 const authRouter = require('./authRouter');
-const timerRouter = require('./timer.route');
+// const timerRouter = require('./timer.route');
 
 router.use('/callRequest', callRequestRouter);
 router.use('/auth', authRouter);
-router.use('/timer', timerRouter);
+// router.use('/timer', timerRouter);
 
 module.exports = router;
